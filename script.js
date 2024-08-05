e4e490a0155febdefa37e9121a756269b47c2d79
ajohnmoses <a.johnmoses95@gmail.com>
2024-08-05 20:12:17 +0530

    major work

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function dismissNotification() {
     var notification = document.getElementById('notification');
     notification.style.display = 'none';
   }
-  const navLinks = document.querySelector('.nav-links')
-  function onToggleMenu(e){
-      e.name = e.name === 'menu' ? 'close' : 'menu'
-      navLinks.classList.toggle('top-[9%]')
-  }
+  function onToggleMenu(menuIcon) {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('top-[80px]');
+    navLinks.classList.toggle('top-[-100%]');
+}

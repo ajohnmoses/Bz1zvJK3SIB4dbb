db87b90b4e32d6c43c930d22a85203aef39a5f29
ajohnmoses <a.johnmoses95@gmail.com>
2024-08-05 20:58:46 +0530

    feat: added animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
         wiggle: {
           '0%, 100%': { transform: 'translateY(-13px)' },
           '50%': { transform: 'translateY(13PX)' },
+        },
+        tintro: {
+          '0%': { opacity : '0' ,transform: 'translateY(25px)' },
+          '100%': { opacity : '1',transform: 'translateY(0PX)' },
+          
         }
       }
     },

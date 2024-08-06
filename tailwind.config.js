0ef99efdb1e10b5c20544b5f069f286967bbb7b0
akirankumar06 <kiranhemu24@gmail.com>
2024-08-06 14:35:55 +0530

    feat: mission and vision heading added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,24 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite",
+      },
       keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" }, // Corrected "translate" to "translateX"
+        },
         wiggle: {
           '0%, 100%': { transform: 'translateY(-13px)' },
-          '50%': { transform: 'translateY(13PX)' },
+          '50%': { transform: 'translateY(13px)' }, // Corrected '13PX' to '13px'
         },
         tintro: {
-          '0%': { opacity : '0' ,transform: 'translateY(25px)' },
-          '100%': { opacity : '1',transform: 'translateY(0PX)' },
-          
-        }
-      }
+          '0%': { opacity: '0', transform: 'translateY(25px)' },
+          '100%': { opacity: '1', transform: 'translateY(0px)' }, // Corrected '0PX' to '0px'
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};

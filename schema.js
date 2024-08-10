b52e5411c2b610c0b0b5315600d3a7431b74a1e4
ajohnmoses <a.johnmoses95@gmail.com>
2024-08-10 16:37:06 +0530

    added seo optimizations

--- schema.js
+++ schema.js
@@ -5,24 +5,20 @@ const schemaData = {
     "name": "Pearl Tech Solutions",  // No change needed here
     "url": "https://www.pearltechsolutions.in",  // Change to your actual website URL
     "logo": "https://pearltechsolutions.in/Assets/Logo.png",  // Change to your actual logo URL
-    "sameAs": [
-      "https://www.facebook.com/yourpage",  // Change to your actual Facebook page URL
-      "https://twitter.com/yourprofile",    // Optionally add or update other social profiles
-      "https://www.linkedin.com/company/yourcompany"  // Optionally add or update other social profiles
-    ],
+    
     "contactPoint": {
       "@type": "ContactPoint",
-      "telephone": "+1-234-567-8901",  // Change to your actual contact number
+      "telephone": "+91 63793 85125",  // Change to your actual contact number
       "contactType": "Customer Service",
-      "email": "info@pearltechsolutions.com"  // Change to your actual email address
+      "email": "thepearl.global@gmail.com"  // Change to your actual email address
     },
     "address": {
       "@type": "PostalAddress",
-      "streetAddress": "123 Tech Street",  // Change to your actual street address
-      "addressLocality": "Tech City",
-      "addressRegion": "Tech State",
-      "postalCode": "12345",
-      "addressCountry": "US"  // Change to your actual country
+      "streetAddress": "Kolathur",  // Change to your actual street address
+      "addressLocality": "Chennai",
+      "addressRegion": "Tamilnadu",
+      "postalCode": "600082",
+      "addressCountry": "India"  // Change to your actual country
     }
   };
   
